d11a1e57799b725fea5361597a346c62bd7eff34
alkachhirang <alkachhirang3@gmail.com>
2023-09-21 19:04:15 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -105,5 +105,5 @@ $('.myslider').slick({
         console.log("Delayed for 5 second.");
     document.getElementById("preloader").classList.add("opacity-0")
     document.getElementById("preloader").classList.add("pointer_event_none")
-    document.body.classList.remove("overflow-hidden")
+    document.body.classList.remove("overflow_hidden")
         }, 5000);
